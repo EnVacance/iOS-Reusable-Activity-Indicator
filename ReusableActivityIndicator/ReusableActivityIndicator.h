b0183279863e033e2d80165e847c4f0ae0fcbdf8
EnVacance <>
2019-03-31 22:49:34 +0900

    Add module ReusableActivityIndicator

---
+++ ReusableActivityIndicator/ReusableActivityIndicator.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReusableActivityIndicator.
+FOUNDATION_EXPORT double ReusableActivityIndicatorVersionNumber;
+
+//! Project version string for ReusableActivityIndicator.
+FOUNDATION_EXPORT const unsigned char ReusableActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReusableActivityIndicator/PublicHeader.h>
+
+
