eb98242be451c1ea122883e54e83e8f19e7b49d4
EnVacance <>
2019-03-31 22:59:01 +0900

    Add start & stop buttons on ViewController
    - Embed ViewController under UINavigationController on Storyboard.
    - Add the buttons as Bar Button Item.
    - Connect IBOutlets for the buttons

--- ReusableActivityIndicatorSample/ViewController.swift
+++ ReusableActivityIndicatorSample/ViewController.swift
@@ -7,7 +7,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
 
+// MARK: - Event
+
+extension ViewController {
+    
+    @IBAction func startButtonTap(_ sender: Any) {
+        print("Start")
+    }
+    
+    @IBAction func stopButtonTap(_ sender: Any) {
+        print("Stop")
+    }
+    
 }
 
