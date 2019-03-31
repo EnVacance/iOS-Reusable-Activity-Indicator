d260484328a548ec6f32909e30f274bff88c3b16
EnVacance <>
2019-03-31 23:04:05 +0900

    Add three sample views
    Add them on storyboard with each having different colors.
    Connect IBOutlet to ViewController.

--- ReusableActivityIndicatorSample/ViewController.swift
+++ ReusableActivityIndicatorSample/ViewController.swift
@@ -3,6 +3,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wideRectangleView: UIView!
+    @IBOutlet weak var tallRectangleView: UIView!
+    @IBOutlet weak var squareView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
