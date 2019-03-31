b7b851de2e85bc12c0ac7638b224469da58079c4
EnVacance <>
2019-03-31 23:06:43 +0900

    Limit access to ReusableActivityIndicatorSettings only via shared instance

--- ReusableActivityIndicator/ReusableActivityIndicatorSettings.swift
+++ ReusableActivityIndicator/ReusableActivityIndicatorSettings.swift
@@ -5,6 +5,10 @@ public class ReusableActivityIndicatorSettings: NSObject {
     
     public static var shared: ReusableActivityIndicatorSettings = ReusableActivityIndicatorSettings()
     
+    override init() {
+        super.init()
+    }
+    
     public var style: UIActivityIndicatorView.Style = .gray
 }
 
